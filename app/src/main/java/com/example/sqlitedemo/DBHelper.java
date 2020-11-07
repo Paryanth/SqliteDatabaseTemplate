1c6f6ce0284dcbf0b4fb1efcbd9e00dcdbd6b50d
Prashanth BS <bsprashanth26@gmail.com>
2020-11-07 09:52:55 +0530

    DBHandler Code is completed- Phase-II

--- app/src/main/java/com/example/sqlitedemo/DBHelper.java
+++ app/src/main/java/com/example/sqlitedemo/DBHelper.java
@@ -2,11 +2,15 @@ package com.example.sqlitedemo;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
     private static final String DBName = "studentdb" ;
@@ -49,4 +53,49 @@ public class DBHelper extends SQLiteOpenHelper {
         db.delete(TBName,place_usn +"=?", new String[]{String.valueOf(student.getUsn())}) ;
         db.close();
     }
+
+    // Update
+    public int updateStudent(Student student){
+        SQLiteDatabase db = this.getWritableDatabase() ; // Open the database in writable format
+        ContentValues cv = new ContentValues() ; // prepare the data as set of key-value pairs
+        cv.put(place_name,student.getName());
+        cv.put(place_usn, student.getUsn());
+        return db.update(TBName,cv,place_usn+="?",new String[]{String.valueOf(student.getUsn())});
+    }
+
+    Student getStudent(int usn){
+        SQLiteDatabase db = this.getReadableDatabase() ;
+
+        Cursor cursor = db.query(
+                TBName, new String[]{place_name,place_usn},place_usn+="?",new String[]{String.valueOf(usn)},null,
+                null,null);
+
+        Student student = new Student(cursor.getString(0), cursor.getInt(1));
+        return student;
+    }
+
+    public int getStudentCount(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TBName, null);
+        cursor.close();
+        return cursor.getCount();
+    }
+
+    public List<Student> getAllStudent(){
+
+        List<Student> studentlist = new ArrayList<Student>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TBName, null);
+        if(cursor.moveToFirst()){
+            do{
+                Student student = new Student();
+                student.setName(cursor.getString(0));
+                student.setUsn(cursor.getInt(1));
+
+                studentlist.add(student);
+            }while (cursor.moveToNext());
+        }
+        return  studentlist;
+    }
+
 }
